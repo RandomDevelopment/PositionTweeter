6c135b928f64fed0af099c94fd43d61a2cb213ef
Nick Foster <me@nickfost.com>
2017-04-23 20:24:23 -0500

    moved this back to init

--- init.php
+++ init.php
@@ -4,4 +4,12 @@ require("vendor/autoload.php");
 require("functions.php");
 
 
+use Medoo\Medoo;
+
+$database = new Medoo([
+	'database_type' => 'sqlite',
+	'database_file' => 'db.db'
+]);
+
+
 ?>
--- test_2.php
+++ test_2.php
@@ -2,18 +2,6 @@
 
 //meedo tester
 require "init.php";
-
-
-use Medoo\Medoo;
-
-$database = new Medoo([
-	'database_type' => 'sqlite',
-	'database_file' => 'db.db'
-]);
-
-
-
-
 print_r($database->info());
 
 
